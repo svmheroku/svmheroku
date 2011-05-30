0932b139c69ddffc999e5f6a8d13321113e88c54
svm heroku <svmheroku@yahoo.com>
2011-05-30 07:34:17 +0100

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Svm::Application.routes.draw do
-  get "r10/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +48,7 @@ Svm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "r10", :action => "index"
+  root :to => "r10", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
