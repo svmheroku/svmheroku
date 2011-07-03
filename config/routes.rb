739119dbf2976381b6bbc53243f000de397bd2b9
svm heroku <svmheroku@yahoo.com>
2011-07-03 02:51:19 +0100

    glossary.html.slim

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Svm::Application.routes.draw do
   match 'contact' => 'r10#contact'
   match 'blog' => 'r10#blog'
   match 'leadership_team' => 'r10#leadership_team'
+  match 'glossary' => 'r10#glossary'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
